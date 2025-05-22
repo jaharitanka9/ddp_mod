9cd621e911236a3f6f23df107ca391667e5240dc
jaharitanka9 <jahari.tanka9@gmail.com>
2025-05-22 15:37:42 +0200

    Update d3-linegraph.js

--- out/html/d3-linegraph.js
+++ out/html/d3-linegraph.js
@@ -16,13 +16,13 @@ function addMonths(date, months) {
 d3.linegraph = function(noTicks, noDots, parties, partyColors, partyNames, dataMax, dataMin, additionalMonths) {
     /* params */
     if (!parties) {
-        parties = ['spd', 'kpd', 'ddp', 'z', 'dvp', 'dnvp', 'nsdap', 'other'];
+        parties = ['wtb', 'kpd', 'onepercent', 'z', 'fuckers', 'dnvp', 'nsdap', 'other'];
     }
     if (!partyColors) {
         partyColors = {'spd': '#E3000F', 'kpd': '#8B0000', 'ddp': '#DCCA4A', 'z': '#000', 'dvp': '#D5AC27', 'dnvp': '#3f7bc1', 'nsdap': '#954B00', 'other': '#a0a0a0'};
     }
     if (!partyNames) {
-        partyNames = {'spd': 'SPD', 'kpd': 'KPD', 'ddp': 'DDP', 'z': 'Z + BVP', 'dvp': 'DVP', 'dnvp': 'DNVP', 'nsdap': 'NSDAP', 'other': 'Others'};
+        partyNames = {'wtb': 'WTB', 'kpd': 'KPD', 'onepercent': '1%', 'z': 'Z + 3%', 'fuckers': 'fuckers', 'dnvp': 'DNVP', 'nsdap': 'NSDAP', 'other': 'Others'};
     }
     if (!additionalMonths) {
         additionalMonths = 10;
